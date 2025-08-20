9df0640629c005ab9331e920ad2f52a65d5df978
I528931 <sagar02@sap.com>
2025-08-21 00:11:32 +0530

    System class

---
+++ day1.java
@@ -0,0 +1,24 @@
+package Java365Days;
+
+public class day1 {
+    public static void main(String[] args){
+
+        //print a string in java
+        System.out.println("Hello world!");
+
+        //print the java version
+        System.out.println("Java Version: "+ System.getProperty("java.version"));
+
+        //print the os version
+        System.out.println("OS Version: " + System.getProperty("os.version"));
+
+        //print java home path
+        System.out.println("Java Home: " + System.getProperty("java.home"));
+
+        //store a string in var
+        var greeting = "Modern java is productive";
+
+        // print var
+        System.out.println(greeting);
+    }
+}
